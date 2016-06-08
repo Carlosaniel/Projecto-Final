09df46136b13fce44ce438222758a3d696a4122d
Carlos Fonseca <carlosaniel@hotmail.com>
2016-06-08 22:05:06 +0100

    Alteração 1
    Mudança do js, adição do menu resized on scroll, botão depois da
    carta...

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,10 @@ $(document).ready(function(){
     $("#carta").animate({
         height: '750px',}, 2000);
     
-    
-    $("#paramais").animate({opacity: '1',}, 10000);
-    
     $("#painelhamb").hide();
     
     $("#hamburguer").click(function(){
             $("#painelhamb").slideToggle("slow");
-    });  
-    
-    $("#dia1").mouseover(function(){
-            $("#descridia1").animate("slow");
     }); 
     
     $("#descridia1").hide();
@@ -33,7 +26,64 @@ $(document).ready(function(){
     
     $("#dia3").click(function(){
         $("#descridia3").animate({width:'toggle'},350);  
-    });    
+    }); 
+    
+    function Scroll(){
+        var top = document.getElementById('header');
+        var ypos = window.pageYOffset;
+            if(ypos < 600) {
+                top.style.height = "55px";
+            }
+            else {
+                top.style.height = "35px";
+            }
+        
+        var top = document.getElementById('botao');
+        var ypos = window.pageYOffset;
+            if(ypos < 600) {
+                top.style.height = "45px";
+                top.style.fontSize = "1em";
+                top.style.paddingTop = "1em";
+            }
+            else {
+                top.style.height = "30px";
+                top.style.fontSize = "0.8em";
+                top.style.paddingTop = "0.6em";
+            }
+        
+        var top = document.getElementById('botao1');
+        var ypos = window.pageYOffset;
+            if(ypos < 600) {
+                top.style.height = "45px";
+                top.style.fontSize = "1em";
+                top.style.paddingTop = "1em";
+            }
+            else {
+                top.style.height = "30px";
+                top.style.fontSize = "0.8em";
+                top.style.paddingTop = "0.6em";
+            }
+        
+        var top = document.getElementById('botao2');
+        var ypos = window.pageYOffset;
+            if(ypos < 600) {
+                top.style.height = "45px";
+                top.style.fontSize = "1em";
+                top.style.paddingTop = "1em";
+            }
+            else {
+                top.style.height = "30px";
+                top.style.fontSize = "0.8em";
+                top.style.paddingTop = "0.6em";
+            }
+        
+        
+        }
+    
+    window.addEventListener("scroll",Scroll);
+    
+    
+    
     
 });
 
