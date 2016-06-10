ecae97442a750993ff27aa1370d539822fdaaf41
Carlos Fonseca <carlosaniel@hotmail.com>
2016-06-10 15:15:13 +0100

    Actualização de todas as medias queries...

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,17 +16,51 @@ $(document).ready(function(){
     $("#descridia3").hide();
     
     
-    $("#dia1").click(function(){
+    $("#dia1").mouseover(function(){
         $("#descridia1").animate({width:'toggle'},350);  
     });  
     
-    $("#dia2").click(function(){
+    $("#dia1").mouseout(function(){
+        $("#descridia1").animate({width:'toggle'},350);  
+    });  
+    
+    $("#dia2").mouseover(function(){
+        $("#descridia2").animate({width:'toggle'},350);  
+    });     
+    
+    $("#dia2").mouseout(function(){
         $("#descridia2").animate({width:'toggle'},350);  
     });   
     
-    $("#dia3").click(function(){
+    $("#dia3").mouseover(function(){
         $("#descridia3").animate({width:'toggle'},350);  
+    });    
+    
+    $("#dia3").mouseout(function(){
+        $("#descridia3").animate({width:'toggle'},350);  
+    });  
+    
+    $("#paramais").mouseover(function(){
+        $("#paramais").css({background:'#D0D0D0'});  
     }); 
+    
+    $("#paramais").mouseout(function(){
+        $("#paramais").css({background:'none'});  
+    });  
+    
+    $("#provedor").hide();
+    
+    $("#question").mouseover(function(){
+        $("#question").css({background: "-webkit-gradient(linear, left top, left bottom, from(#3A5FCD), to(#27408B)"});  
+    });      
+    
+    $("#question").mouseout(function(){
+        $("#question").css({background: "#3A5FCD"}); 
+    });      
+    
+    $("#question").click(function(){
+        $("#provedor").slideToggle(1000);
+    });  
     
     function Scroll(){
         var top = document.getElementById('header');
@@ -41,7 +75,7 @@ $(document).ready(function(){
         var top = document.getElementById('botao');
         var ypos = window.pageYOffset;
             if(ypos < 600) {
-                top.style.height = "45px";
+                top.style.height = "51px";
                 top.style.fontSize = "1em";
                 top.style.paddingTop = "1em";
             }
@@ -54,7 +88,7 @@ $(document).ready(function(){
         var top = document.getElementById('botao1');
         var ypos = window.pageYOffset;
             if(ypos < 600) {
-                top.style.height = "45px";
+                top.style.height = "51px";
                 top.style.fontSize = "1em";
                 top.style.paddingTop = "1em";
             }
@@ -67,7 +101,7 @@ $(document).ready(function(){
         var top = document.getElementById('botao2');
         var ypos = window.pageYOffset;
             if(ypos < 600) {
-                top.style.height = "45px";
+                top.style.height = "51px";
                 top.style.fontSize = "1em";
                 top.style.paddingTop = "1em";
             }
